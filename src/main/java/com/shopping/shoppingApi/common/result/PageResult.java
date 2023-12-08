7110379bbd5211e312257fb7650c5b77180d18fd
wg-233 <1208361250@qq.com>
2023-12-08 16:50:11 +0800

    refactor: 修改分页参数

--- src/main/java/com/shopping/shoppingApi/common/result/PageResult.java
+++ src/main/java/com/shopping/shoppingApi/common/result/PageResult.java
@@ -12,21 +12,21 @@ public class PageResult<T> implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
     @Schema(description = "总记录数")
-    private int counts;
+    private long counts;
     @Schema(description = "分页条数")
     private int pageSize;
     @Schema(description = "当前页码")
     private int page;
     @Schema(description = "总页数")
-    private int pages;
+    private long pages;
     @Schema(description = "列表数据")
     private List<T> list;
 
-    public PageResult(long counts, int page, long pages, List<T> list) {
-        this.counts = (int) counts;
-        this.pageSize = 36;
+    public PageResult(long counts,int pageSize, int page, long pages, List<T> list) {
+        this.counts = counts;
+        this.pageSize = pageSize;
         this.page = page;
-        this.pages = (int) pages;
+        this.pages = pages;
         this.list = list;
     }
 
