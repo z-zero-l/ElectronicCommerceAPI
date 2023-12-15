0dbdf12c806bd353b728d166e87c2e31411db1ce
wg-233 <1208361250@qq.com>
2023-12-15 09:39:12 +0800

    fix(地址模块):修复地址更新问题

--- src/main/java/com/shopping/shoppingApi/service/impl/AddressServiceImpl.java
+++ src/main/java/com/shopping/shoppingApi/service/impl/AddressServiceImpl.java
@@ -51,28 +51,28 @@ public class AddressServiceImpl extends ServiceImpl<AddressMapper, Address> impl
      */
     @Override
     public Void addAddress(Integer userId, AddressVO addressVO) {
-        if (addressVO.getReceiver()==null) {
+        if (addressVO.getReceiver() == null) {
             throw new RuntimeException("收件人不能为空");
         }
-        if (addressVO.getContact()==null) {
+        if (addressVO.getContact() == null) {
             throw new RuntimeException("联系方式不能为空");
         }
-        if (addressVO.getProvinceCode()==null) {
+        if (addressVO.getProvinceCode() == null) {
             throw new RuntimeException("省份编码不能为空");
         }
-        if (addressVO.getCityCode()==null) {
+        if (addressVO.getCityCode() == null) {
             throw new RuntimeException("城市编码不能为空");
         }
-        if (addressVO.getDistrictCode()==null) {
+        if (addressVO.getDistrictCode() == null) {
             throw new RuntimeException("区县编码不能为空");
         }
-        if (addressVO.getAddress()==null) {
+        if (addressVO.getAddress() == null) {
             throw new RuntimeException("详细地址不能为空");
         }
-        if (addressVO.getIsDefault()==null) {
+        if (addressVO.getIsDefault() == null) {
             addressVO.setIsDefault(0);
-        }else {
-            if (addressVO.getIsDefault()==1) {
+        } else {
+            if (addressVO.getIsDefault() == 1) {
                 Address address = getOne(QueryChain.create().where(ADDRESS.USER_ID.eq(userId)).and(ADDRESS.IS_DEFAULT.eq(1)));
                 address.setIsDefault(0);
                 address.update();
@@ -101,37 +101,40 @@ public class AddressServiceImpl extends ServiceImpl<AddressMapper, Address> impl
      */
     @Override
     public Void updateAddress(Integer userId, AddressVO addressVO) {
-        if(!exists(QueryChain.create().where(ADDRESS.ID.eq(addressVO.getId())).and(ADDRESS.USER_ID.eq(userId)))){
+        if (!exists(QueryChain.create().where(ADDRESS.ID.eq(addressVO.getId())).and(ADDRESS.USER_ID.eq(userId)))) {
             throw new RuntimeException("地址不存在");
         }
-        if (addressVO.getReceiver()==null) {
+        if (addressVO.getReceiver() == null) {
             throw new RuntimeException("收件人不能为空");
         }
-        if (addressVO.getContact()==null) {
+        if (addressVO.getContact() == null) {
             throw new RuntimeException("联系方式不能为空");
         }
-        if (addressVO.getProvinceCode()==null) {
+        if (addressVO.getProvinceCode() == null) {
             throw new RuntimeException("省份编码不能为空");
         }
-        if (addressVO.getCityCode()==null) {
+        if (addressVO.getCityCode() == null) {
             throw new RuntimeException("城市编码不能为空");
         }
-        if (addressVO.getDistrictCode()==null) {
+        if (addressVO.getDistrictCode() == null) {
             throw new RuntimeException("区县编码不能为空");
         }
-        if (addressVO.getAddress()==null) {
+        if (addressVO.getAddress() == null) {
             throw new RuntimeException("详细地址不能为空");
         }
-        if (addressVO.getIsDefault()==null) {
+        if (addressVO.getIsDefault() == null) {
             addressVO.setIsDefault(0);
-        }else {
-            if (addressVO.getIsDefault()==1) {
+        } else {
+            if (addressVO.getIsDefault() == 1) {
                 Address address = getOne(QueryChain.create().where(ADDRESS.USER_ID.eq(userId)).and(ADDRESS.IS_DEFAULT.eq(1)));
-                address.setIsDefault(0);
-                address.update();
+                if (address != null) {
+                    address.setIsDefault(0);
+                    address.update();
+                }
             }
         }
         Address address = Address.create()
+                .setId(addressVO.getId())
                 .setUserId(userId)
                 .setReceiver(addressVO.getReceiver())
                 .setContact(addressVO.getContact())
@@ -152,9 +155,9 @@ public class AddressServiceImpl extends ServiceImpl<AddressMapper, Address> impl
      */
     @Override
     public Void deleteAddress(Integer userId, Integer addressId) {
-        if(!exists(QueryChain.create().where(ADDRESS.ID.eq(addressId)).and(ADDRESS.USER_ID.eq(userId)))){
+        if (!exists(QueryChain.create().where(ADDRESS.ID.eq(addressId)).and(ADDRESS.USER_ID.eq(userId)))) {
             throw new RuntimeException("地址不存在");
-        }else {
+        } else {
             if (!removeById(addressId)) {
                 throw new RuntimeException("删除地址失败");
             }
